11eea6ebf4a9465260a4538ced6584210b418188
kianoushit1999 <kianoushit1999@gmail.com>
2024-09-15 11:50:31 +0330

    adding form via ts

--- dist/app.js
+++ dist/app.js
@@ -1,2 +1,27 @@
 "use strict";
+class ProjectInput {
+    constructor() {
+        this.templateEl = document.getElementById("project-input");
+        this.hostInput = document.getElementById("app");
+        const el = document.importNode(this.templateEl.content, true);
+        this.element = el.firstElementChild;
+        this.element.id = "user-input";
+        this.titleEl = this.element.querySelector("#title");
+        this.descEl = this.element.querySelector("#description");
+        this.peopleEl = this.element.querySelector("#people");
+        this.Configure();
+        this.attach();
+    }
+    submitHandler(event) {
+        event.preventDefault();
+        console.log(this.titleEl.value);
+    }
+    Configure() {
+        this.element.addEventListener('submit', this.submitHandler.bind(this));
+    }
+    attach() {
+        this.hostInput.insertAdjacentElement("afterbegin", this.element);
+    }
+}
+const prIn = new ProjectInput();
 //# sourceMappingURL=app.js.map
--- src/app.ts
+++ src/app.ts
@@ -1,1 +1,39 @@
 // Code goes here!
+class ProjectInput {
+  templateEl: HTMLTemplateElement;
+  hostInput: HTMLTemplateElement;
+  element: HTMLFormElement;
+  titleEl: HTMLInputElement;
+  descEl: HTMLInputElement;
+  peopleEl: HTMLInputElement;
+
+  constructor() {
+    this.templateEl = document.getElementById(
+      "project-input"
+    )! as HTMLTemplateElement;
+    this.hostInput = document.getElementById("app")! as HTMLTemplateElement;
+    const el = document.importNode(this.templateEl.content, true);
+    this.element = el.firstElementChild as HTMLFormElement;
+    this.element.id = "user-input"
+    this.titleEl = this.element.querySelector("#title")!;
+    this.descEl = this.element.querySelector("#description")!;
+    this.peopleEl = this.element.querySelector("#people")!;
+    this.Configure()
+    this.attach();
+  }
+
+  private submitHandler(event:Event) {
+    event.preventDefault()
+    console.log(this.titleEl.value)
+  }
+
+  private Configure() {
+    this.element.addEventListener('submit', this.submitHandler.bind(this))
+  }
+
+  private attach() {
+    this.hostInput.insertAdjacentElement("afterbegin", this.element);
+  }
+}
+
+const prIn = new ProjectInput();
